758cba1550d2720350082d3b8ff3f2312d7bc5c6
Fleurdel34 <betdevelweb0953@gmail.com>
2023-03-31 16:52:36 +0200

    Exercice cours React

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import {Banner, Cart} from './Banner';
+import ShoppingList from './ShoppingList';
+
+function App() {
+    return (<div>
+            <Banner />
+            <Cart />
+            <ShoppingList />
+            </div>);
+}
+
+export default App;
