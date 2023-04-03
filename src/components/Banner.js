c433d0047d220ff5dcb097c07d63a2360a7bb02f
Fleurdel34 <betdevelweb0953@gmail.com>
2023-04-03 09:37:09 +0200

    Avancer des exercices

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,30 +1,7 @@
-import logo from '../assets/logo.png';
-import '../Styles/Banner.css';
+import '../styles/Banner.css'
 
-
-
-export function Banner() {
-    const title = 'La maison jungle'
-    return (
-        <div className='lmj-banner'>
-            <img src={logo} alt='La maison jungle' className='lmj-logo' />
-            <h1 className='lmj-title'>{title}</h1>
-        </div>)
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
-
-
-export function Cart() {
-    const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
-    return (<div className='lmj-cart'>
-        <h2>Panier</h2>
-        <ul>
-        <li>Monstera : {monsteraPrice}€</li>
-         <li>Lierre : {ivyPrice}€</li>
-         <li>Fleurs : {flowerPrice}€</li>
-        </ul>
-          Total : {monsteraPrice + ivyPrice + flowerPrice }€
-          </div>)
-}
+export default Banner
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,35 +1,34 @@
-import { plantList } from '../datas/plantList';
-import '../Styles/ShoppingList.css';
-
-
+import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-    const categories = plantList.reduce(
+	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
 
 	return (
-		<div>
+		<div className='lmj-shopping-list'>
 			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-                        {plant.name}{plant.isSpecialOffer && <span className='lmj-sales'> Soldes! </span>}
-					</li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						key={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
-		
-                    
-              
 	)
 }
 
 export default ShoppingList
-
